c90b84f714cd14249bce8491a6ec16f94e7be5e5
therealstyle <vaniasidorenko@gmail.com>
2021-05-09 12:40:04 +0300

    Extension time!
    add fileformats.ts and I extends with official_music.ts

--- src/gd/official_music.ts
+++ src/gd/official_music.ts
@@ -1,12 +1,17 @@
+import { fileformat } from "fileformats";
+
 export type Music = {
 	id: number;
 	name: string;
 	filename: string;
+	file_format: string;
 }
 
 let MusicList = [
-	{ id: 0, name: "Stay Inside Me", filename: "StayInsideMe.mp3" },
-	{ id: 1, name: "Stereo Medness", filename: "StereoMadness.mp3" },
-	{ id: 2, name: "Back On Track", filename: "BackOnTrack.mp3" },
-
+	{ id: 0, name: "Stay Inside Me", filename: `StayInsideMe${fileformat}` },
+	{ id: 1, name: "Stereo Medness", filename: `StereoMadness${fileformat}` },
+	{ id: 2, name: "Back On Track", filename: `BackOnTrack${fileformat}` },
+	{ id: 3, name: "Dry Out", filename: `DryOut${fileformat}` },
+	{ id: 4, name: "Base After Base", filename: `BaseAfterBase${fileformat}` },
+	{ id: 5, name: "Cant let Go", filename: `CantLetGo${fileformat}` }
 ]
---
+++ src/system/fileformats.ts
@@ -0,0 +1,9 @@
+enum Format {
+	mp3,
+	ogg,
+	plist,
+	png,
+	dll
+}
+
+let fileformat: string = `.${Format}`;
