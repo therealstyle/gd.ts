9a0d725a0da945a7df1270c8e7bd379809cfbfa5
therealstyle <vaniasidorenko@gmail.com>
2021-05-09 12:23:40 +0300

    official_music
    Add weird list. Bruh

---
+++ src/gd/official_music.ts
@@ -0,0 +1,12 @@
+export type Music = {
+	id: number;
+	name: string;
+	filename: string;
+}
+
+let MusicList = [
+	{ id: 0, name: "Stay Inside Me", filename: "StayInsideMe.mp3" },
+	{ id: 1, name: "Stereo Medness", filename: "StereoMadness.mp3" },
+	{ id: 2, name: "Back On Track", filename: "BackOnTrack.mp3" },
+
+]
